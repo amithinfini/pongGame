373307312a7a4df04ab685e3bdad1c6cc1f160f3
amithinfini <amith.infini@gmail.com>
2022-01-16 01:58:35 +0530

    testing score board

--- test.py
+++ test.py
@@ -27,6 +27,16 @@ ball.penup()  # turtles draw a line as they move, so we dont need to do it
 ball.goto(330, 0)
 
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)  # this is the speed of animation
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 20, "normal"))
+
+
 # Main game loop
 while True:
     wn.update()
