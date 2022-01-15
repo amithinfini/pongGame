6b22b006e8ed7c4eee4d7a1b63c4943fa2e722a1
amithinfini <amith.infini@gmail.com>
2022-01-16 00:44:27 +0530

    Created Window and Paddles

---
+++ pongGame.py
@@ -0,0 +1,37 @@
+# Pong Game
+
+import turtle
+
+wn = turtle.Screen()  # creating the window
+wn.title("Pong by @amithinfini")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)  # to manually update, so the game is fast
+
+
+# Paddle A
+paddleA = turtle.Turtle()
+paddleA.speed(0)  # this is the speed of animation
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_wid=5, stretch_len=1)
+paddleA.penup()  # turtles draw a line as they move, so we dont need to do it
+paddleA.goto(-350, 0)
+
+
+# Paddle B
+paddleB = turtle.Turtle()
+paddleB.speed(0)  # this is the speed of animation
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_wid=5, stretch_len=1)
+paddleB.penup()  # turtles draw a line as they move, so we dont need to do it
+paddleB.goto(350, 0)
+
+
+# Ball
+
+
+# Main game loop
+while True:
+    wn.update()
